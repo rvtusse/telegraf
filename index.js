2ecb18c2d0f060be0843a402089de88fbc551f69
Digital_academy_intern <herve@thedigitalacademy.co.za>
2018-12-06 20:41:54 +0200

    first commit

--- index.js
+++ index.js
@@ -10,38 +10,6 @@ var firebaseHelper = require('firebase-functions-helper')
 //var _ = require('lodash');
 
 
-//Firebase initialization
-
-
-admin.initializeApp({
-    credential: admin.credential.applicationDefault()
- });
- const db = admin.firestore()
- 
- // creating my collection
-
- const userDetailsCollections = 'userDetails';
- const userIntentsCollections = 'userIntents';
-
-
-
- //add userDetails database
-
- // Add a new document with a generated id.
-var addDoc = db.collection('userDetails').add({
-    name: 'Tokyo',
-    country: 'Japan'
-  }).then(ref => {
-    console.log('Added document with ID: ', ref.id);
-  });
-
-
-
-//get a database reference to the database
-
-// var db = admin.database();
-// var ref = db.ref("/");
-
 
 var bb = require('bot-brother')
 var bot = bb({
@@ -54,14 +22,14 @@ var bot = bb({
 //chat action typing
 
 bot.command('typing')
-    .invoke(function(ctx) {
-      var USERID = ctx.meta.user.id
-      var action = "typing";
-      bot.api.sendChatAction(USERID, action).then(function (resp) {
-       
-       return ctx.go('getintent')
-      })
-   
+    .invoke(function (ctx) {
+        var USERID = ctx.meta.user.id
+        var action = "typing";
+        bot.api.sendChatAction(USERID, action).then(function (resp) {
+
+            return ctx.go('getintent')
+        })
+
     });
 
 
@@ -84,24 +52,18 @@ bot.command('getintent').invoke(function (ctx) {
     return axios.get('http://516d0ec3.ngrok.io/processor/v1/userIntents/' + ctx.message.contact.phone_number)
         .then((response) => {
 
-
             //startingKeyboard(response.data)
             console.log('i am here')
             console.log(response.data)
 
         })
-}) 
-
-
-
-
+})
 
 
 //Registration
 
 
 bot.command('start').invoke(function (ctx) {
-
     var option = {
         "parse_mode": "Markdown",
         "reply_markup": {
@@ -110,17 +72,41 @@ bot.command('start').invoke(function (ctx) {
                 text: "My phone number",
                 request_contact: true
             }], ["Cancel"]]
-            
+
         }
     };
- 
-  return ctx.sendMessage("Y'ello " + ctx.meta.user.first_name + "," + " may you please register with your phone number", option )
-})
-.answer(function (ctx) {
 
-  return ctx.go('intent')
+    return ctx.sendMessage("Y'ello " + ctx.meta.user.first_name + "," + " may you please register with your phone number", option)
 })
-   
+//calling addUserDetails
+addUserDetails(ctx)
+
+    .answer(function (ctx) {
+        return ctx.go('intent')
+    })
+
+
+// store users number 
+
+function addUserDetails(ctx) {
+    let userData = {
+
+        userData: ctx.message.contact.phone_number
+    }
+
+
+    //posting data to the processor endpoint
+
+    axios.post('', userData)
+        .then(function (response) {
+            console.log(response.data);
+
+        })
+        .catch(function (error) {
+
+        });
+}
+
 
 
 
@@ -134,18 +120,37 @@ bot.command('intent')
         ctx.session.memory = ctx.session.memory || '';
         ctx.session.memory += ctx.answer + ',';
         ctx.data.memory = ctx.session.memory;
-
-
         //console.log(ctx.message.contact.phone_number);
 
-
-        // addUserIntent(ctx);
+    //calling addUserIntent
+        addUserIntent(ctx);
 
         return ctx.sendMessage('Thanks, your request is being processed');
     })
     .answer(function (ctx) {
         return ctx.go('Confirmation');
     });
+
+// store users intent
+
+function addUserIntent(ctx) {
+    let userIntents = {
+
+        userIntents: ctx.session.memory
+    }
+
+
+    //posting data to the processor endpoint
+
+    axios.post('', userIntents)
+        .then(function (response) {
+            console.log(response.data);
+
+        })
+        .catch(function (error) {
+
+        });
+}
 
 //confirmation prompt message
 
@@ -162,85 +167,67 @@ bot.command('Confirmation')
         return ctx.sendMessage('Your answer is <%=answer%>');
     });
 
-    //Yes command (Existing user keyboard)
-    bot.command('default')
+//Yes command (Existing user keyboard)
+
+bot.command('default')
     .invoke(function (ctx) {
-        return ctx.sendMessage('Yoh! ' + ctx.meta.user.first_name +',' + ' what would you like to do now?')
+        return ctx.sendMessage('Yoh! ' + ctx.meta.user.first_name + ',' + ' what would you like to do now?')
     })
     .keyboard([
         [{ 'saved intent': { go: 'typingt' } }],
         [{ 'New intent': { go: 'intent' } }],
-        [{ 'Chit-chat': {go: 'typepromos' }}]
+        [{ 'Chit-chat': { go: 'typepromos' } }]
     ])
     .answer(function (ctx) {
         ctx.data.answer = ctx.answer;
-      
+
     });
 
 
-
 //chat action typing 1 ONE 
-   
+
 bot.command('typepromos')
-.invoke(function(ctx) {
-  var USERID = ctx.meta.user.id
-  var action = "typing";
-  bot.api.sendChatAction(USERID, action).then(function (resp) {
-   
-   return ctx.go('promos')
-  })
-
-});
-
-
-  //promo  (getting promotional messages from the processor)
-
-
-
-    bot.command('promos')
-    .invoke(function(ctx) {
-        return axios.get('http://ec100a85.ngrok.io/processor/v1/promotions')
-        .then((response) => {
-           
-
-            console.log('i am here')
-            console.log(response.data)
-            //ctx.sendMessage(ctx.meta.user.first_name + '  did you know about this ?')
-            ctx.sendMessage(response.data.advert)
-            return ctx.go('chitchat')
+    .invoke(function (ctx) {
+        var USERID = ctx.meta.user.id
+        var action = "typing";
+        bot.api.sendChatAction(USERID, action).then(function (resp) {
+            return ctx.go('promos')
         })
+
+    });
+
+
+//promo  (getting promotional messages from the processor)
+
+bot.command('promos')
+    .invoke(function (ctx) {
+        return axios.get('http://ec100a85.ngrok.io/processor/v1/promotions')
+            .then((response) => {
+
+
+                console.log('i am here')
+                console.log(response.data)
+                //ctx.sendMessage(ctx.meta.user.first_name + '  did you know about this ?')
+                ctx.sendMessage(response.data.advert)
+                return ctx.go('chitchat')
+            })
 
     })
 
 
+//chitchat command 
 
-// //chat action typing 2 TWo
-   
-// bot.command('typechitchat')
-// .invoke(function(ctx) {
-//   var USERID = ctx.meta.user.id
-//   var action = "typing";
-//   bot.api.sendChatAction(USERID, action).then(function (resp) {
-   
-//    return ctx.go('chitchat')
-//   })
-
-// });
-
-
-    //chitchat command 
-
-    bot.command('chitchat')
-    .invoke((ctx)=>{
+bot.command('chitchat')
+    .invoke((ctx) => {
         return ctx.sendMessage('Sawubona ' + ctx.meta.user.first_name)
     })
 
 
-        //bye commmand (closing a sessiom)
+//bye commmand (closing a sessiom)
 
-    bot.command('bye').invoke(function (ctx) {
-        return ctx.sendMessage('Bye ' + ctx.meta.user.first_name);
-    });
+bot.command('bye').invoke(function (ctx) {
+    return ctx.sendMessage('Bye ' + ctx.meta.user.first_name);
+});
 
 
 
@@ -254,6 +241,7 @@ function addUserIntent(ctx) {
 
 
     //posting data to the processor endpoint
+
     axios.post('http://0da912ca.ngrok.io/processor/v1/actionRequest', userData)
         .then(function (response) {
             console.log(response.data);
@@ -262,18 +250,5 @@ function addUserIntent(ctx) {
         .catch(function (error) {
 
         });
-
-    // // Add a new 'users' item
-    // var itemsRef = ref.child("userIntent ");
-    // var newItemRef = itemsRef.push();
-    // newItemRef.set({
-    //     "msidn": ctx.meta.user.id,
-    //     "user Intent": ctx.session.memory,
-    //     "intent Created Time": new Date().toString()
-    // });
-
-    // var itemId = newItemRef.key;
-    // console.log("userID and intent " + itemId + " is successfully created.");
-    // return itemId;
 }
 
