39a1f8a03ec50976cd6e8a66e80e0cb88ddfea16
Nhlanhla <digital_acdeamy@Nhlanhlas-MacBook-Air.local>
2018-12-18 08:35:16 +0200

    first commit

--- scenes/savedIntentScene.js
+++ scenes/savedIntentScene.js
@@ -6,20 +6,13 @@ const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup');
 //const util = require('util')
 
-//THIS IS A KEYBOARD FUNCTION
-function startingKeyboard(menu) {
-    defaultKeyboard = menu
-    console.log('This is the keyboard function')
-    bot.keyboard(defaultKeyboard)
 
-}
 // GET STORED USER INTENT FROM THE PROCCESOR
 savedIntentScene.enter((ctx) => {
     axios.get('http://560cd184.ngrok.io/processor/v1/userIntents/' + ctx.session.contact_number )
         .then(response => {
 
             //CALLING KEYBOARD FUNCTION
-            //startingKeyboard(response.data)
             console.log(response);
             ctx.reply(JSON.stringify(response.data.exists));
             //ctx.scene.enter('')
@@ -27,7 +20,7 @@ savedIntentScene.enter((ctx) => {
             ctx.reply('Would you like to do something else?', Markup
             .keyboard([
 
-                response
+                response.data.exists
             
             ])
             .oneTime()
@@ -39,12 +32,5 @@ savedIntentScene.enter((ctx) => {
 
 })
 
-
-/*
-.catch((err) => {
-    console.log('Sorry there was a problem please try again later', err)
-    ctx.scene.enter('greetingScene');
-})
-*/
 module.exports = savedIntentScene;
 
